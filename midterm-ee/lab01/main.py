68cf4d79bc333585cd9b67c9c60c77c6b5e79028
vivobook2024 <jyang@stut.edu.tw>
2024-04-16 06:22:33 +0800

    home

---
+++ midterm-ee/lab01/main.py
@@ -0,0 +1,9 @@
+def digits_in_ascending_order(number):
+    # replace pass with your code
+    pass
+
+
+if __name__ == "__main__":
+    number = int(input("Enter a three-digit integer: "))
+    print(digits_in_ascending_order(number))
+
